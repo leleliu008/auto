a2d38e44cfb00e1f8b8e369c93792ffbef63e270
leleliu008 <leleliu008@leleliu008deMacBook-Pro.local>
2017-01-21 15:41:25 +0800

    优化

--- vim-setup/setup.sh
+++ vim-setup/setup.sh
@@ -1,52 +1,81 @@
-#!/bin/sh
+#!/bin/bash
 
-osType=`uname -s`
-echo "osType=$osType"
+function installBrewOnMacOSX() {
+    which brew
+    if [ $? -eq 0 ] ; then
+        echo "brew already installed!"
+    else
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+}
 
-if [ $osType == "Darwin" ] ; then
-        which brew
-        if [ $? -eq 0 ] ; then
-                echo "brew already installed!"
-        else
-                /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        fi
+function installVimOnMacOSX() {
+    which vim
+    if [ $? -eq 0 ] ; then
+        echo "vim already installed!"
+    else
+        brew install vim
+    fi
+}
 
-        which vim
-        if[ $? -eq 0 ] ; then
-                echo "vim already installed!"
-        else
-                brew install vim
-        fi
+function installCurlOnMacOSX() {
+    which curl
+    if [ $? -eq 0 ] ; then
+        echo "curl already installed!"
+    else
+        brew install curl
+    fi
+}
 
-        which curl
-        if [ $? -eq 0 ] ; then
-                echo "curl already installed!"
-        else
-                brew install curl
-        fi
-elif [ $osType == "Linux" ] ; then
-        if [ -f '/etc/debain-release' ] ; then
-                sudo apt-get install curl
-                sudo apt-get install vim
-                sudo apt-get install exuberant-ctags
+function installOnUbuntu() {
+    sudo apt-get install curl
+    sudo apt-get install vim
+    sudo apt-get install exuberant-ctags
+}
+
+function installOnCentOS() {
+    sudo yum install curl
+    sudo yum install vim
+    sudo yum install ctags-etags
+}
+
+function updateVimrc() {
+    if [ -f '~/.vimrc' ] ; then
+        mv ~/.vimrc ~/.vimrc.bak
+    fi
+    cp vimrc-user ~/.vimrc
+
+    echo "---------------------------------------------------"
+    echo "~/.vimrc config file is updated! "
+    echo "your ~/.vimrc config file is bak to ~/.vimrc.bak"
+    echo "open vim and use :BundleInstall to install plugins!"
+    echo "---------------------------------------------------"
+}
+
+function main() {
+    osType=`uname -s`
+    echo "osType=$osType"
+
+    if [ $osType = "Darwin" ] ; then
+        installBrewOnMacOSX
+        installVimOnMacOSX
+        installCurlOnMacOSX
+    elif [ $osType = "Linux" ] ; then
+        if [ -f '/etc/lsb-release' ] ; then
+            installOnUbuntu
         elif [ -f '/etc/redhat-release' ] ; then
-                sudo yum install curl
-                sudo yum install vim
-                sudo yum install ctags-etags
+            installOnCentOS
         fi
-fi
+    fi
 
-if [ -f 'vimrc-user'] ; then
-        if [ -f '~/.vimrc' ] ; then
-                mv ~/.vimrc ~/.vimrc.bak
-        fi
-        cp vimrc-user ~/.vimrc
-else
+    if [ -f 'vimrc-user' ] ; then
+        updateVimrc
+    else
         curl -O https://raw.githubusercontent.com/leleliu008/auto/master/vim-setup/vimrc-user
         if [ $? -eq 0 ] ; then
-                if [ -f '~/.vimrc' ] ; then
-                    mv ~/.vimrc ~/.vimrc.bak
-                fi
-                cp vimrc-user ~/.vimrc
+            updateVimrc
         fi
-fi
+    fi
+}
+
+main

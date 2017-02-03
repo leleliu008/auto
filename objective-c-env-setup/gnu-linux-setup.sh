3ff22a5035bcb29f791d5ef54184e7c1a11f2afc
leleliu008 <leleliu008@leleliu008deMacBook-Pro.local>
2017-02-04 06:38:34 +0800

    添加Objective-C换件搭建搭建脚本

---
+++ objective-c-env-setup/gnu-linux-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+role=""
+if [ `whoami` != "root" ] ; then
+    role=sudo
+fi
+
+# 在Ubuntu中安装GNUstep的v1版（不支持Objectie-C 2.0）
+function installGNUstepV1OnUbuntu() {
+    $role apt-get install gobjc
+    $role apt-get install gnustep
+    $role apt-get install gnustep-devel
+    $role bash /usr/share/GNUstep/Makefiles/GNUstep.sh
+}
+
+function main() {
+    if [ `uname -s` = "Darwin" ] ; then
+        echo "your os is not GNU/Linux";
+        exit 1
+    else
+        if [ -f "/etc/lsb-release" ] ; then
+            installGNUstepV1OnUbuntu
+        elif [ -f "/etc/redhat-release" ] ; then
+            echo "do nothing"
+        fi
+    fi
+}
+
+main

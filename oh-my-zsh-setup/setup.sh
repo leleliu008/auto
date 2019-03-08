b41cbb6530c48c2876e2d714233378c0fc175514
leleliu008 <792793182@qq.com>
2019-03-08 18:34:41 +0800

    添加Zsh配置环境

--- oh-my-zsh-setup/setup.sh
+++ oh-my-zsh-setup/setup.sh
@@ -2,28 +2,27 @@
 
 function installOhMyZsh() {
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && \
-    git -C ~/.oh-my-zsh/plugins https://github.com/zsh-users/zsh-syntax-highlighting.git && \
-    git -C ~/.oh-my-zsh/plugins https://github.com/zsh-users/zsh-autosuggestions.git && \
-    git -C ~/.oh-my-zsh/plugins https://github.com/zsh-users/zsh-completions.git && \
+    git -C ~/.oh-my-zsh/plugins clone https://github.com/zsh-users/zsh-syntax-highlighting.git && \
+    git -C ~/.oh-my-zsh/plugins clone https://github.com/zsh-users/zsh-autosuggestions.git && \
+    git -C ~/.oh-my-zsh/plugins clone https://github.com/zsh-users/zsh-completions.git
 
 }
 
 function main() {
-    if [ -f "$HOME/.oh-my-zsh" ] ; then
-        echo "OhMyZsh has installed!"
+    if [ -f "~/.oh-my-zsh" ] ; then
+        echo "Oh-My-Zsh has already installed!"
         exit 1
     fi
 
-    #Docker容器的默认是用户是root，且没有安装sudo命令
-    sudo=`which sudo`
-    osType=`uname -s`
+    local sudo=`which sudo`;
+    local osType=`uname -s`;
 
     if [ "$osType" == "Linux" ] ; then
         # 如果是Ubuntu系统
         if [ -f "/etc/lsb-release" ] ; then
             $sudo apt-get update && \
             $sudo apt-get install -y curl git zsh && \
-            installOhMyZsh
+            $installOhMyZsh
         # 如果是CentOS系统
         elif [ -f "/etc/redhat-release" ] ; then
             $sudo yum update && \

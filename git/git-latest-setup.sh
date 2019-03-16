f29a18c54ddf0ac2116e194db045c8c4b669c136
leleliu008 <792793182@qq.com>
2019-03-16 20:51:00 +0800

    优化

--- git/git-latest-setup.sh
+++ git/git-latest-setup.sh
@@ -27,24 +27,35 @@ if [ "`which curl 2> /dev/null`" == "" ] ; then
     fi
 fi
 
-SHELL=`echo $SHELL`
-if [ "$SHELL" == "" ] ; then
-    SHELL=`awk -F: '{print $7}' /etc/passwd`
-fi
-SHELL=`basename $SHELL`
-echo "currentShell=$SHELL"
-
 URL=https://mirrors.edge.kernel.org/pub/software/scm/git
 latestFileName=`curl -L "$URL" | grep "git-[0-9].[0-9].[0-9].tar.[x|g]z" | awk -F\" '{print $2}' | awk 'END{print}'`
-curl -LO "$URL/$latestFileName" && \
-cd `tar -tf $latestFileName | sed -n "1p"` && \
-make prefix=/usr/local/git all && \
-make prefix=/usr/local/git install
+fileExtension=`echo "$latestFileName" | awk -F "." '{print $NF}'`
 
-if [ "$SHELL" == "bash" ] ; then
-    echo "export PATH=$PATH:/usr/local/git/bin" >> ~/.bashrc
-    source ~/.bashrc
-elif [ "$SHELL" == "zsh" ] ; then
-    echo "export PATH=$PATH:/usr/local/git/bin" >> ~/.zshrc
-    source ~/.zshrc
+curl -LO "$URL/$latestFileName";
+
+if [ $? -eq 0 ] ; then
+    if [ "$fileExtension" == "gz" ] ; then
+        tar zvxf "$latestFileName"
+    elif [ "$fileExtension" == "xz" ] ; then
+        tar Jvxf "$latestFileName"
+    fi
+
+    cd `tar -tf $latestFileName | sed -n "1p"` && \
+    make prefix=/usr/local/git all && \
+    make prefix=/usr/local/git install
+
+    SHELL=`echo $SHELL`
+    if [ "$SHELL" == "" ] ; then
+        SHELL=`awk -F: '{print $7}' /etc/passwd`
+    fi
+    SHELL=`basename $SHELL`
+    echo "current shell is $SHELL";
+    
+    if [ "$SHELL" == "bash" ] ; then
+        echo "export PATH=$PATH:/usr/local/git/bin" >> ~/.bashrc
+        source ~/.bashrc
+    elif [ "$SHELL" == "zsh" ] ; then
+        echo "export PATH=$PATH:/usr/local/git/bin" >> ~/.zshrc
+        source ~/.zshrc
+    fi
 fi

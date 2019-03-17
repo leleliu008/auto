860a90026a9d6cd52e0084c53dd49fddee383e28
leleliu008 <792793182@qq.com>
2019-03-17 13:20:23 +0800

    优化

--- git/git-latest-setup.sh
+++ git/git-latest-setup.sh
@@ -28,14 +28,11 @@ URL=https://mirrors.edge.kernel.org/pub/software/scm/git
 latestFileName=`curl -sSL# "$URL" | grep "git-[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}.tar.xz" | awk -F\" '{print $2}' | sort -V | awk 'END{print}'`;
 fileExtension=`echo "$latestFileName" | awk -F "." '{print $NF}'`;
 
-[ -f "$latestFileName" ] && rm -rf "$latestFileName";
-
-echo -e "\e[37;31;1mthe latest git version is $latestFileName\e[39;49;0m";
-echo -e "\e[37;31;1mdownloading $URL/$latestFileName\e[39;49;0m";
-
-curl -LO "$URL/$latestFileName";
-
-if [ $? -eq 0 ] ; then
+[ -f "$latestFileName" ] && \
+rm -rf "$latestFileName" && \
+echo -e "\e[37;31;1mthe latest git version is $latestFileName\e[39;49;0m" && \
+echo -e "\e[37;31;1mdownloading $URL/$latestFileName\e[39;49;0m" && \
+curl -LO "$URL/$latestFileName" && {
     if [ "$fileExtension" == "gz" ] ; then
         tar zvxf "$latestFileName"
     elif [ "$fileExtension" == "xz" ] ; then
@@ -44,21 +41,7 @@ if [ $? -eq 0 ] ; then
 
     cd `tar -tf $latestFileName | sed -n "1p"` && \
     make prefix=/usr/local/git all && \
-    make prefix=/usr/local/git install || {
-        exit 1
-    }
-
-    SHELL=`echo $SHELL`
-    if [ "$SHELL" == "" ] ; then
-        SHELL=`awk -F: '{print $7}' /etc/passwd`
-    fi
-    SHELL=`basename $SHELL`
-     
-    if [ "$SHELL" == "bash" ] ; then
-        echo "export PATH=/usr/local/git/bin:\$PATH" >> ~/.bashrc
-        source ~/.bashrc
-    elif [ "$SHELL" == "zsh" ] ; then
-        echo -e "\e[37;32;1mgit has been installed into /usr/local/git, please execute command as follow in your terminal:\e[39;49;0m"
-        echo -e "\e[37;32;1mecho \"export PATH=/usr/local/git/bin:\$PATH\" >> ~/.zshrc\e[39;49;0m"
-    fi
-fi
+    make prefix=/usr/local/git install && \
+    $sudo -s /usr/local/git/bin/git /usr/local/bin/git && \
+    echo -e "\e[37;32;1mgit has been installed into /usr/local/git\e[39;49;0m"
+}

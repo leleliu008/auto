44fb0f49df41061469f411f8d80ffb79d4429ac2
leleliu008 <792793182@qq.com>
2019-08-24 19:34:44 +0800

    optimize

--- vim-setup/setup.sh
+++ vim-setup/setup.sh
@@ -2,14 +2,12 @@
 
 currentScriptDir="$(cd $(dirname $0); pwd)"
 
-Black='\033[0;30m'        # Black
 Red='\033[0;31m'          # Red
 Green='\033[0;32m'        # Green
 Yellow='\033[0;33m'       # Yellow
 Blue='\033[0;34m'         # Blue
 Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
 
 Color_off='\033[0m'       # Text Reset
 
@@ -22,7 +20,7 @@ function success() {
 }
 
 function info() {
-    msg "${Blue}[➭]${Color_off} ${1}${2}"
+    msg "${Purple}[➭]${Color_off} ${1}${2}"
 }
 
 function warn() {

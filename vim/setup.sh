791f8f64ce865b8dcbf3baa9d4911bbd2a524811
leleliu008 <792793182@qq.com>
2020-07-26 21:20:47 +0800

    optimize

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,270 +1,219 @@
 #!/bin/sh
 
-export GO111MODULE=on
-export GOPROXY=https://goproxy.io
 
 currentScriptDir="$(cd "$(dirname "$0")" || exit; pwd)"
-osType="$(uname -s)"
-[ "$(whoami)" = "root" ] || sudo=sudo
 
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Purple='\033[0;35m'       # Purple
+[ "$(whoami)" != "root" ] && command -v sudo > /dev/null && sudo=sudo
 
-Color_off='\033[0m'       # Text Reset
+Color_Red='\033[0;31m'          # Red
+Color_Green='\033[0;32m'        # Green
+Color_Yellow='\033[0;33m'       # Yellow
+Color_Purple='\033[0;35m'       # Purple
+Color_Off='\033[0m'             # Reset
 
 msg() {
-    printf '%b\n' "$1" >&2
+    printf "%b" "$*"
 }
 
-success() {
-    msg "${Green}[✔]${Color_off} ${1}${2}"
+echo() {
+    msg "$*\n"
 }
 
 info() {
-    msg "${Purple}[❉]${Color_off} ${1}${2}"
+    msg "${Color_Purple}$*\n${Color_Off}"
+}
+
+success() {
+    msg "${Color_Green}[✔] $*\n${Color_Off}"
 }
 
 warn() {
-    msg "${Yellow}[⌘]${Color_off} ${1}${2}"
+    msg "${Color_Yellow}🔥 $*\n${Color_Off}"
 }
 
 error() {
-    msg "${Red}[✘]${Color_off} ${1}${2}"
+    msg "${Color_Red}[✘] $*\n${Color_Off}"
 }
 
-installHomeBrewIfNeeded() {
-    if command -v brew > /dev/null ; then
-        brew update
+die() {
+    msg "${Color_Red}[✘] $*\n${Color_Off}"
+    exit 1
+}
+
+sed_in_place() {
+    if command -v gsed > /dev/null ; then
+        gsed -i "$1" "$2"
+    elif command -v sed  > /dev/null ; then
+        sed -i    "$1" "$2" 2> /dev/null ||
+        sed -i "" "$1" "$2"
     else
-        printf "\n\n" | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        die "please install sed utility."
     fi
 }
 
-checkDependency() {
-    command -v "$1" > /dev/null || pkgNames="$pkgNames $2"
+check_dependency() {
+    command -v "$1" > /dev/null || PKG_LIST="$PKG_LIST $2"
 }
 
-checkDependencies() {
-    command -v emerge > /dev/null && {
-        command -v zsh > /dev/null || {
-            command -v bash > /dev/null || pkgNames="app-shells/zsh"
-        }
-        command -v curl  > /dev/null || pkgNames="$pkgNames net-misc/curl"
-        command -v git   > /dev/null || pkgNames="$pkgNames dev-vcs/git"
-        command -v vim   > /dev/null || pkgNames="$pkgNames app-editors/vim"
-        command -v sed   > /dev/null || pkgNames="$pkgNames sys-apps/sed"
-        command -v tar   > /dev/null || pkgNames="$pkgNames app-arch/tar"
-        command -v gawk  > /dev/null || pkgNames="$pkgNames sys-apps/gawk"
-        command -v gzip  > /dev/null || pkgNames="$pkgNames app-arch/gzip"
-        command -v make  > /dev/null || pkgNames="$pkgNames sys-devel/make"
-        command -v cmake > /dev/null || pkgNames="$pkgNames dev-util/cmake"
-        command -v go    > /dev/null || pkgNames="$pkgNames dev-lang/go"
-        command -v ninja > /dev/null || pkgNames="$pkgNames dev-util/ninja"
-        command -v ctags > /dev/null || pkgNames="$pkgNames dev-util/ctags"
-        command -v python > /dev/null || pkgNames="$pkgNames dev-lang/python"
-        return 0
-    }
-
-    checkDependency bash bash
-    checkDependency curl curl
-    checkDependency git git
-    checkDependency vim vim
-    checkDependency sed sed
-    checkDependency gawk gawk
-    checkDependency gzip gzip
-    checkDependency make make
-    checkDependency cmake cmake
-
-    if [ "$osType" = "Darwin" ] ; then
-        checkDependency go go
-        checkDependency ninja ninja
-        checkDependency ctags ctags
-        checkDependency python3 python3
-        checkDependency tar gnu-tar
-    else
-        checkDependency tar tar
-
-        command -v pacman > /dev/null && {
-            checkDependency go go
-            checkDependency g++ gcc
-            checkDependency ninja ninja
-            checkDependency ctags ctags
-            checkDependency python3 python
-            return 0
-        }
-        
-        command -v apk > /dev/null && {
-            checkDependency go go
-            checkDependency g++ gcc
-            checkDependency ninja ninja
-            checkDependency ctags ctags
-            checkDependency python3 python3
-            pkgNames="$pkgNames python3-dev"
-            return 0
-        }
-        
-        command -v apt-get > /dev/null && {
-            checkDependency go golang
-            checkDependency g++ g++
-            checkDependency ninja ninja-build
-            checkDependency ctags exuberant-ctags
-            checkDependency python3 python3 
-            pkgNames="$pkgNames python3-dev"
-            return 0
-        }
-        
-        command -v dnf > /dev/null && {
-            checkDependency go golang
-            checkDependency g++ gcc-c++
-            checkDependency ninja ninja-build
-            checkDependency ctags ctags-etags
-            checkDependency python3 python3 
-            pkgNames="$pkgNames python3-devel"
-            return 0
-        }
-        
-        command -v yum > /dev/null && {
-            checkDependency go golang
-            checkDependency g++ gcc-c++
-            checkDependency ninja ninja-build
-            checkDependency ctags ctags-etags
-            checkDependency python3 python36 
-            pkgNames="$pkgNames python36-devel"
-            return 0
-        }
-        
-        command -v zypper > /dev/null && {
-            checkDependency go go
-            checkDependency g++ gcc-c++
-            checkDependency ninja ninja
-            checkDependency ctags ctags
-            checkDependency python3 python3
-            pkgNames="$pkgNames python3-devel"
-        }
-    fi
+check_dependencies() {
+    echo "$@" | while read -r item
+    do
+        check_dependency "$item"
+    done
 }
 
-installDependencies() {
-    if [ "$osType" = "Darwin" ] ; then
-        installHomeBrewIfNeeded &&
-        brew install $@
-        return $?
-    elif [ "$osType" = "Linux" ] ; then
-        # Gentoo Linux
-        command -v emerge > /dev/null && {
-            $sudo emerge $@
-            return $?
-        }
-        
-        # ArchLinux、ManjaroLinux
-        command -v pacman > /dev/null && {
+install_dependencies_if_needed() {
+    # Gentoo Linux
+    if command -v emerge > /dev/null ; then
+        check_dependencies "bash app-shells/bash\ncurl net-misc/curl\ngit dev-vcs/git\nvim app-editors/vim\nsed sys-apps/sed\ntar app-arch/tar\ngawk sys-apps/gawk\ngzip app-arch/gzip\nmake sys-devel/make\ncmake dev-util/cmake\ngo dev-lang/go\nninja dev-util/ninja\nctags dev-util/ctags\npython dev-lang/python"
+        PKG_LIST=$(echo "$PKG_LIST" | sed 's/^ //g')
+        if [ -n "$PKG_LIST" ] ; then
+            $sudo emerge $PKG_LIST
+        fi
+    # macOS GNU/Linux
+    elif command -v brew > /dev/null ; then
+        check_dependencies "bash bash\ncurl curl\ngit git\nvim vim\nsed sed\ngawk gawk\ngzip gzip\nmake make\ncmake cmake\ngo go\nninja ninja\nctags ctags\npython3 python3\ntar gnu-tar"
+        PKG_LIST=$(echo "$PKG_LIST" | sed 's/^ //g')
+        if [ -n "$PKG_LIST" ] ; then
+            brew install $PKG_LIST
+        fi
+    # ArchLinux、ManjaroLinux
+    elif command -v pacman > /dev/null ; then
+        check_dependencies "bash bash\ncurl curl\ngit git\nvim vim\nsed sed\ngawk gawk\ngzip gzip\nmake make\ncmake cmake\ntar tar\ngo go\ng++ gcc\nninja ninja\nctags ctags\npython3 python" 
+        PKG_LIST=$(echo "$PKG_LIST" | sed 's/^ //g')
+        if [ -n "$PKG_LIST" ] ; then
             $sudo pacman -Syyuu --noconfirm &&
-            $sudo pacman -S     --noconfirm $@
-            return $?
-        }
-        
-        # AlpineLinux
-        command -v apk > /dev/null && {
+            $sudo pacman -S     --noconfirm $PKG_LIST
+        fi
+    # AlpineLinux
+    elif command -v apk > /dev/null ; then
+        check_dependencies "bash bash\ncurl curl\ngit git\nvim vim\nsed sed\ngawk gawk\ngzip gzip\nmake make\ncmake cmake\ngo go\ng++ gcc\nninja ninja\nctags ctags\npython3 python3"
+        PKG_LIST="$PKG_LIST python3-dev"
+        PKG_LIST=$(echo "$PKG_LIST" | sed 's/^ //g')
+        if [ -n "$PKG_LIST" ] ; then
             $sudo apk update &&
-            $sudo apk add $@
-            return $?
-        }
-        
-        # Debian GNU/Linux系
-        command -v apt-get > /dev/null && {
+            $sudo apk add $PKG_LIST
+        fi
+    # Debian GNU/Linux系
+    elif command -v apt-get > /dev/null ; then
+        check_dependencies "bash bash\ncurl curl\ngit git\nvim vim\nsed sed\ngawk gawk\ngzip gzip\nmake make\ncmake cmake\ngo golang\ng++ g++\nninja ninja-build\nctags exuberant-ctags\npython3 python3"
+        PKG_LIST="$PKG_LIST python3-dev"
+        PKG_LIST=$(echo "$PKG_LIST" | sed 's/^ //g')
+        if [ -n "$PKG_LIST" ] ; then
             $sudo apt-get -y update &&
-            $sudo apt-get -y install $@
-            return $?
-        }
-        
-        # Fedora、CentOS8
-        command -v dnf > /dev/null && {
+            $sudo apt-get -y install $PKG_LIST
+        fi
+    # Fedora、CentOS8
+    elif command -v dnf > /dev/null ; then
+        check_dependencies "bash bash\ncurl curl\ngit git\nvim vim\nsed sed\ngawk gawk\ngzip gzip\nmake make\ncmake cmake\ngo golang\ng++ gcc-c++\nninja ninja-build\nctags ctags-etags\npython3 python3"
+        PKG_LIST="$PKG_LIST python3-devel"
+        PKG_LIST=$(echo "$PKG_LIST" | sed 's/^ //g')
+        if [ -n "$PKG_LIST" ] ; then
             $sudo dnf -y update &&
-            $sudo dnf -y install $@
-            return $?
-        }
-        
-        # RHEL CentOS 7、6
-        command -v yum > /dev/null && {
+            $sudo dnf -y install $PKG_LIST
+        fi
+    # RHEL CentOS 7、6
+    elif command -v yum > /dev/null ; then
+        check_dependencies "bash bash\ncurl curl\ngit git\nvim vim\nsed sed\ngawk gawk\ngzip gzip\nmake make\ncmake cmake\ngo golang\ng++ gcc-c++\nninja ninja-build\nctags ctags-etags\npython3 python36"
+        PKG_LIST="$PKG_LIST python36-devel"
+        PKG_LIST=$(echo "$PKG_LIST" | sed 's/^ //g')
+        if [ -n "$PKG_LIST" ] ; then
             $sudo yum -y update &&
             $sudo yum -y install epel-release
-            $sudo yum -y install $@
-            return $?
-        }
-        
-        # OpenSUSE
-        command -v zypper > /dev/null && {
+            $sudo yum -y install $PKG_LIST
+        fi
+    # OpenSUSE
+    elif command -v zypper > /dev/null ; then
+        check_dependencies "bash bash\ncurl curl\ngit git\nvim vim\nsed sed\ngawk gawk\ngzip gzip\nmake make\ncmake cmake\ngo go\ng++ gcc-c++\nninja ninja\nctags ctags\npython3 python3"
+        PKG_LIST="$PKG_LIST python3-devel"
+        PKG_LIST=$(echo "$PKG_LIST" | sed 's/^ //g')
+        if [ -n "$PKG_LIST" ] ; then
             $sudo zypper update  -y &&
-            $sudo zypper install -y $@
-            return $?
-        }
+            $sudo zypper install -y $PKG_LIST
+        fi
     fi
 }
 
-installVimPlug() {
-    info "Installing vim-plug..." &&
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
-    success "Installed vim-plug"
+install_vim_plug() {
+    info "Installing vim-plug..."
+
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    if [ $? -eq 0 ] ; then
+        success "vim-plug installed success."
+    else
+        die "vim-plug installed fialed."
+    fi
 }
 
-compileYouCompleteMe() {
+compile_YCM() {
     info "Compiling YouCompleteMe..."
-    $python install.py $@ --ninja || {
+    $PYTHON install.py $@ --ninja || {
         info "ReCompiling YouCompleteMe..."
-        $python install.py $@
+        $PYTHON install.py $@
     }
 }
 
-installYouCompleteMe() {
-    pluginDir="${HOME}/.vim/bundle"
-    youCompleteMeDir="${pluginDir}/YouCompleteMe"
+install_YCM() {
+    VIM_PLUGIN_DIR="$HOME/.vim/bundle"
+    YCM_DIR="$VIM_PLUGIN_DIR/YouCompleteMe"
+     
+    [ -d "$VIM_PLUGIN_DIR" ] || mkdir -p "$VIM_PLUGIN_DIR"
+    [ -d "$YCM_DIR" ] && rm -rf "$YCM_DIR"
     
-    [ -d "$pluginDir" ] || mkdir -p "$pluginDir"
-    [ -d "$youCompleteMeDir" ] && rm -rf "$youCompleteMeDir"
-    
-    export GOPATH=$youCompleteMeDir/go
+    export GO111MODULE=on
+    export GOPROXY=https://goproxy.io
+    export GOPATH=$YCM_DIR/go
         
     info "Installing YouCompleteMe..."
-    git clone --recursive https://gitee.com/YouCompleteMe/YouCompleteMe.git "$youCompleteMeDir" &&
-    cd "$youCompleteMeDir" && {
-        python="$(command -v python3)"
-        [ -z "$python" ] && python="$(command -v python)"
-
-        if [ -z "$python" ] ; then
-            warn "we can't find python, so don't compile installYouCompleteMe, you can comiple it by hand"
-        else
-            options="--clang-completer --ts-completer --go-completer"
-            
-            command -v java > /dev/null && {
-                options="$options --java-completer"
-                sedCompatible "s@download.eclipse.org@mirrors.ustc.edu.cn/eclipse@g" ./third_party/ycmd/build.py
-            }
-            
-            if compileYouCompleteMe "$options" ; then
-                success "installed YouCompleteMe"
-            else
-                warn "compiled failed!you can comiple it by hand"
-            fi
-        fi
+    git clone --recursive https://gitee.com/YouCompleteMe/YouCompleteMe.git "$YCM_DIR" &&
+    cd "$YCM_DIR" || exit 1
+    
+    PYTHON=$(command -v python3) ||
+    PYTHON=$(command -v python)  ||
+    PYTHON=$(command -v python2) ||
+    die  "we can't find python, so don't compile YouCompleteMe, you can comiple it by hand"
+    
+    OPTION_LIST="--clang-completer --ts-completer --go-completer"
+    
+    command -v java > /dev/null && {
+        OPTION_LIST="$OPTION_LIST --java-completer"
+        sed_in_place "s@download.eclipse.org@mirrors.ustc.edu.cn/eclipse@g" ./third_party/ycmd/build.py
     }
+    
+    if compile_YCM "$OPTION_LIST" ; then
+        success "YouCompleteMe installed success."
+    else
+        die "YouCompleteMe installed failed! you can comiple it by hand yourself."
+    fi
 }
 
-installNodeJSIfNeeded() {
-    command -v node > /dev/null && 
+install_nvm() {
+    info "Installing nvm..."
+    
+    if curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash ; then
+        export NVM_DIR="${HOME}/.nvm" &&
+        . ~/.nvm/nvm.sh &&
+        success "nvm installed success."
+    else
+        die "nvm installed failed."
+    fi
+}
+
+install_nodejs() {
+    info "Installing node.js v10.15.1"
+    
+    if nvm install v10.15.1 ; then
+        success "node.js v10.15.1 installed success."
+    else
+        die "node.js v10.15.1 installed failed."
+    fi
+}
+
+install_nodejs_if_needed() {
     command -v npm  > /dev/null || {
-        command -v nvm > /dev/null || {
-            info "Installing nvm..." &&
-            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash &&
-            export NVM_DIR="${HOME}/.nvm" &&
-            . ~/.nvm/nvm.sh &&
-            success "Installed nvm"
-        }
-        
-        info "Installing node.js v10.15.1" &&
-        nvm install v10.15.1 &&
-        success "Installed node.js v10.15.1"
+        command -v nvm > /dev/null || install_nvm
+        install_nodejs
     }
     
     if [ "$(npm config get registry)" = "https://registry.npmjs.org/" ] ; then
@@ -272,55 +221,28 @@ installNodeJSIfNeeded() {
     fi
 }
 
-updateVimrcOfCurrentUser() {
-    myVIMRC="${HOME}/.vimrc"
-    [ -f "$myVIMRC" ] && {
-        if [ -f "${myVIMRC}.bak" ] ; then
-            for i in $(seq 1 1000)
-            do
-                if [ -f "${myVIMRC}${i}.bak" ] ; then
-                    continue
-                else
-                    backup="${myVIMRC}${i}.bak"
-                    break
-                fi
-            done
-        fi
-        [ -z "$backup" ] && backup="${myVIMRC}.bak"
-        mv "$myVIMRC" "$backup"
-    }
-    
-    cp "$currentScriptDir/vimrc-user" "$myVIMRC"
-    cp "$currentScriptDir/.tern-project" "${HOME}"
-    
-    [ -z "$python" ] || sedCompatible "s@/usr/local/bin/python3@${python}@g" "$myVIMRC"
-    
-    success "---------------------------------------------------"
-    [ -z "$backup" ] || {
-        success "$HOME/.vimrc config file is updated!"
-        success "your $HOME/.vimrc config file is bak to $backup"
-    }
-    success "open vim and use :PlugInstall command to install plugins!"
-    success "---------------------------------------------------"
-}
+update_vimrc() {
+    VIMRC="$HOME/.vimrc"
 
-sedCompatible() {
-    if [ "$osType" = "Darwin" ] ; then
-        sed -i ""  "$1" "$2"
-    else
-        sed -i "$1" "$2"
-    fi
+    [ -f "$VIMRC" ] && {
+        VIMRC_BAK="$VIMRC.$(date +%s).bak"
+        mv "$VIMRC" "$VIMRC_BAK"
+        warn "$VIMRC has been backup to $VIMRC_BAK"
+    }
+    
+    cp "$currentScriptDir/vimrc-user" "$VIMRC"
+     
+    sed_in_place "s@/usr/local/bin/python3@$PYTHON@g" "$VIMRC"
+     
+    success "All Done. open vim and use :PlugInstall command to install plugins!"
 }
 
 main() {
-    checkDependencies
-    
-    [ -z "$pkgNames" ] || installDependencies "$pkgNames"
-    
-    installVimPlug &&
-    installNodeJSIfNeeded &&
-    installYouCompleteMe &&
-    updateVimrcOfCurrentUser
+    install_dependencies_if_needed &&
+    install_nodejs_if_needed &&
+    install_vim_plug &&
+    install_YCM &&
+    update_vimrc
 }
 
 main

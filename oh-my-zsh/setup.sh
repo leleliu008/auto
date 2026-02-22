9c940904f03e07e444e1552dc0562dcd9d543252
leleliu008 <leleliu008@gmail.com>
2026-02-22 15:52:25 +0800

    remove unused files

--- oh-my-zsh/setup.sh
+++ oh-my-zsh/setup.sh
@@ -12,19 +12,31 @@ gsed -i '/exec zsh -l/d' oh-my-zsh-installer.sh
 
 sh oh-my-zsh-installer.sh
 
+####################################################################
+
+cd ~/.oh-my-zsh/themes/
+
 # https://draculatheme.com/zsh
 git clone --depth=1 https://github.com/dracula/zsh dracula-zsh-theme
 
-mv dracula-zsh-theme/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
-mv dracula-zsh-theme/lib               ~/.oh-my-zsh/themes/
+mv dracula-zsh-theme/dracula.zsh-theme .
+mv dracula-zsh-theme/lib               .
+rm dracula-zsh-theme
 
-git -C ~/.oh-my-zsh/plugins clone https://github.com/zsh-users/zsh-syntax-highlighting
-git -C ~/.oh-my-zsh/plugins clone https://github.com/zsh-users/zsh-autosuggestions
-git -C ~/.oh-my-zsh/plugins clone https://github.com/zsh-users/zsh-completions
+####################################################################
+
+cd ~/.oh-my-zsh/plugins/
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions
+
+####################################################################
 
 gsed -i '/plugins=(/s|)| zsh-syntax-highlighting zsh-autosuggestions zsh-completions)|' ~/.zshrc
 gsed -i '/compinit/d'  ~/.zshrc
 gsed -i '/ZSH_THEME=/c ZSH_THEME="dracula"' ~/.zshrc
+
 printf "autoload -U compinit && compinit\n" >> ~/.zshrc
 
 env zsh -l
